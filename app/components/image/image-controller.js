eb86d400dc46d773b6ea2c7ad362afdc233c5c10
Darryl Kilzer <darrylkilzer@gmail.com>
2017-05-07 10:38:42 -0600

    quote controller update

--- app/components/image/image-controller.js
+++ app/components/image/image-controller.js
@@ -2,19 +2,7 @@ function ImageController() {
 
 	var imageService = new ImageService();
 
-	function update(imageData) {
-		// 	var css = `body {background-image: ${imagedata.url.value}`
-		// 	var head = document.head || document.getElementsByTagName('head')[0]
-		// 	var style = document.createElement('style');
-
-		// 	style.type = 'text/css';
-		// 	if (style.styleSheet) {
-		// 		style.styleSheet.cssText = css;
-		// 	} else {
-		// 		style.appendChild(document.createTextNode(css));
-		// 	}
-		// 	head.appendChild(style);
-		
+	function update(imageData) {		
 		$('#body').css('background-image', `url(${imageData.url})`);
 	}
 
--- app/components/quote/quote-controller.js
+++ app/components/quote/quote-controller.js
@@ -3,9 +3,12 @@ function QuoteController() {
 	var qs = new QuoteService()
 
 	function update(quoteData) {
+		var elem = document.getElementById("quote")
+		elem.innerHTML = quoteData.quote
 	}
 
 	qs.getQuote(function (quoteData) {
+		update(quoteData)
 		console.log('What is the quote', quote)
 	})
 }
