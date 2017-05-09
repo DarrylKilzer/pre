35870cea95f9764b5641148f403c293525309264
Darryl Kilzer <darrylkilzer@gmail.com>
2017-05-09 09:03:02 -0600

    organizing layout

--- app/components/todo/todo-controller.js
+++ app/components/todo/todo-controller.js
@@ -1,21 +1,45 @@
-function TodoController(){
+function TodoController() {
 	// new up the TodoService that has already been configured for your use
 	// There are two methods getTodos returns and array
 	// saveTodos accepts an array and stores it to your local storage
-	var todoService = new TodoService()
+	var todoService = new TodoService();
 
-	this.addTodoFromForm = function(e) {
-		e.preventDefault()
-		// TAKE THE INFORMATION FORM THE FORM
-
-		//ADD IT TO THE ARRAY BELOW
-		var todosArry = todoService.getTodos()
-
-
-
-
-		// FINAL ACTION OF ADDING A TODO
-		todoService.saveTodos(todosArry)
+	function drawTodos(data) {
+		var elem = document.getElementById('todo');
+		var template = '';
+		var todoTemplate = '';
+		for (var i = 0; i < data.length; i++) {
+			var todo = data[i];
+			template += `
+					${todo} <button type="button" onclick="app.controllers.todoController.removeTodo('${todo}')"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button><br>
+				`
+		}
+		if (data.length > 0) {
+			var todos = data.length;
+			todoTemplate = `
+			<h5>To do: ${todos}</h5>`
+		}
+		return elem.innerHTML = template + todoTemplate;
 	}
 
+	this.addTodoFromForm = function (e) {
+		e.preventDefault();
+		var form = e.target;
+
+		var todosArry = todoService.getTodos(form.todo.value);
+		todosArry.push(form.todo.value);
+		todoService.saveTodos(todosArry);
+		drawTodos(todosArry);
+	}
+
+	this.uncheck = function uncheck(todo) {
+		//debugger
+		for (var i = 0; i < todosArray.length; i++) {
+			var currentTodo = todosArray[i]
+			if (todo == currentTodo) {
+				todosArray.splice(currentTodo, 1);
+			}
+		}
+		drawTodos(todosArray);
+	}
 }
--- app/components/weather/weather-controller.js
+++ app/components/weather/weather-controller.js
@@ -6,14 +6,15 @@ function WeatherController() {
 		var weatherTemplate = ""
 		var weather = document.getElementById("weather")
 		weather.innerHTML = `
+		<strong><h2>
 		${weatherData.name}: 
-		 ${Math.round(weatherData.main.temp * 9 / 5 - 459.67)}`
+		 ${Math.round(weatherData.main.temp * 9 / 5 - 459.67)}
+		 </h2></strong>`
 	}
 
 	weatherService.getWeather(function (weatherData) {
 		update(weatherData)
 		console.log(weatherData);
-		//What can you do with this weather object?
 	})
 
 }
